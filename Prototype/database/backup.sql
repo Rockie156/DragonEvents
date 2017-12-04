da84833a93bfec66f6cbbe08971dfe63885a7ef2
Saffat Hasan <sh3292@drexel.edu>
2017-12-04 15:21:40 -0500

    Inserted more data; Deleted event_vw (duplicate)

--- Prototype/database/backup.sql
+++ Prototype/database/backup.sql
@@ -25,6 +25,7 @@ DROP TABLE IF EXISTS `event_tags`;
 CREATE TABLE `event_tags` (
   `event_id` int(11) NOT NULL,
   `tag_id` int(11) NOT NULL,
+  PRIMARY KEY (`event_id`,`tag_id`),
   KEY `event_id` (`event_id`),
   KEY `tag_id` (`tag_id`),
   CONSTRAINT `event_tags_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`),
@@ -38,30 +39,9 @@ CREATE TABLE `event_tags` (
 
 LOCK TABLES `event_tags` WRITE;
 /*!40000 ALTER TABLE `event_tags` DISABLE KEYS */;
-INSERT INTO `event_tags` VALUES (1,1),(1,2),(1,3),(3,1),(3,3);
+INSERT INTO `event_tags` VALUES (1,1),(1,2),(1,3),(3,1),(3,3),(4,1),(4,2),(5,1);
 /*!40000 ALTER TABLE `event_tags` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Temporary table structure for view `event_vw`
---
-
-DROP TABLE IF EXISTS `event_vw`;
-/*!50001 DROP VIEW IF EXISTS `event_vw`*/;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-/*!50001 CREATE TABLE `event_vw` (
-  `event_id` tinyint NOT NULL,
-  `event_name` tinyint NOT NULL,
-  `start_time` tinyint NOT NULL,
-  `end_time` tinyint NOT NULL,
-  `location` tinyint NOT NULL,
-  `tags` tinyint NOT NULL,
-  `first_name` tinyint NOT NULL,
-  `last_name` tinyint NOT NULL,
-  `email` tinyint NOT NULL
-) ENGINE=MyISAM */;
-SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `events`
@@ -79,7 +59,7 @@ CREATE TABLE `events` (
   `location` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,7 +68,7 @@ CREATE TABLE `events` (
 
 LOCK TABLES `events` WRITE;
 /*!40000 ALTER TABLE `events` DISABLE KEYS */;
-INSERT INTO `events` VALUES (1,'Free Food and Music at Drexel!','2017-11-29 15:11:53','2017-11-29 15:11:53',1,'3141 Chestnut St'),(3,'Drexel CAB quizzos','2017-11-30 17:06:54','2017-11-30 17:06:54',2,'Drexel Recreation Center');
+INSERT INTO `events` VALUES (1,'Free Food and Music at Drexel!','2017-11-29 15:11:53','2017-11-29 15:11:53',1,'PISB 101'),(3,'Drexel CAB quizzos','2017-11-30 17:06:54','2017-11-30 17:06:54',2,'Drexel Recreation Center'),(4,'Dance Practice','2017-12-04 15:14:32','2017-12-04 15:14:32',3,'Nesbitt Hall'),(5,'Dragon Jedi Practice','2017-12-04 15:14:32','2017-12-04 15:14:32',4,'Main Building');
 /*!40000 ALTER TABLE `events` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -150,7 +130,7 @@ CREATE TABLE `users` (
   `last_name` varchar(64) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -159,28 +139,9 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Saffat','Hasan','sh3292@drexel.edu'),(2,'Rahul','Godoba','rpg46@drexel.edu');
+INSERT INTO `users` VALUES (1,'Saffat','Hasan','sh3292@drexel.edu'),(2,'Rahul','Godoba','rpg46@drexel.edu'),(3,'Haley','Lee','hl655@drexel.edu'),(4,'Harley','Sacramento','hjs63@drexel.edu'),(5,'Nicholas','Nguyen','nan64@drexel.edu');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Final view structure for view `event_vw`
---
-
-/*!50001 DROP TABLE IF EXISTS `event_vw`*/;
-/*!50001 DROP VIEW IF EXISTS `event_vw`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8 */;
-/*!50001 SET character_set_results     = utf8 */;
-/*!50001 SET collation_connection      = utf8_general_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `event_vw` AS select `e`.`id` AS `event_id`,`e`.`name` AS `event_name`,`e`.`start_time` AS `start_time`,`e`.`end_time` AS `end_time`,`e`.`location` AS `location`,group_concat(`t`.`title` separator ',') AS `tags`,`u`.`first_name` AS `first_name`,`u`.`last_name` AS `last_name`,`u`.`email` AS `email` from (((`events` `e` join `event_tags` `et` on((`e`.`id` = `et`.`event_id`))) join `tags` `t` on((`t`.`id` = `et`.`tag_id`))) join `users` `u` on((`e`.`user_id` = `u`.`id`))) group by `e`.`id` */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
 
 --
 -- Final view structure for view `events_vw`
@@ -210,4 +171,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-30 17:38:42
+-- Dump completed on 2017-12-04 15:20:45
