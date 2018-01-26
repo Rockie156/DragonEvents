009ffff4b28e640dd64fdf0b03108346bdbd9aa3
Saffat Hasan <saffathasan@gmail.com>
2018-01-26 00:29:38 -0500

    Reorganized files and updated server.js to issue 404 on invalid url

--- Prototype/server.js
+++ Prototype/server.js
@@ -1,19 +1,24 @@
 var http = require('http');
 
 var fs = require('fs');
+
 http.createServer(function (req, res){
     if(req.url === '/index.html'){
-        fs.readFile('index.html',function(err,data) {
+        fs.readFile('templates/index.html',function(err,data) {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data);
             res.end();
         });
     }
-    else if(req.url === '/css.css') {
-        fs.readFile('css.css',function(err,data) {
+    else if(req.url === '/index.css') {
+        fs.readFile('css/index.css',function(err,data) {
             res.writeHead(200, {'Content-Type': "text/css"});
             res.write(data);
             res.end();
         });
-    }
+    } else {
+		res.writeHead(404);
+		res.write('Page not found.');
+		res.end();
+	}
 }).listen(8080);
