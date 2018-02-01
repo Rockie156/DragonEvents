51857cf1df51903e07e65f16bc4272c75d793e0e
Saffat Hasan <saffathasan@gmail.com>
2018-01-31 23:58:03 -0500

    Added routes and static to server.js; created index.pug

--- Prototype/server.js
+++ Prototype/server.js
@@ -3,21 +3,33 @@ var fs = require('fs');
 var express = require('express');
 var pug = require('pug');
 var database = require('./firebase.js');
+require('express-session');
 
 app = express();
 app.set('view engine', 'pug');
+app.use(express.static('.'));
+
 
 app.get('/test_firebase', function(req, res) {
-	var events = [];
 	var db = database.get_connection();
 	db.then(function(snapshot) {
 		console.log(snapshot.val().events);
 		res.render('db_test', {"events": snapshot.val().events});
+		res.end();
 	});
 });
 
-app.listen(7080, function () {
-    console.log('listening on port', 7080);
+app.get('/', function(req, res) {
+	var db = database.get_connection();
+	db.then(function(snapshot) {
+		console.log(snapshot.val().events);
+		res.render('index', {"events": snapshot.val().events});
+		res.end();
+	});
+});
+
+app.listen(80, function () {
+    console.log('listening on port', 80);
 });
 
 http.createServer(function (req, res){
@@ -51,6 +63,12 @@ http.createServer(function (req, res){
             res.write(data);
             res.end();
         });
+	} else if(req.url === '/auth.html') {
+		fs.readFile('templates/auth.html', function(err,data) {
+			res.writeHead(200, {'Content-Type':"text/html"});
+			res.write(data);
+			res.end();
+		});
 	} else {
 		res.writeHead(404);
 		res.write('Page not found.');
