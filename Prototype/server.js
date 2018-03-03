3bdc55a4ad5820f27a451531233760f4df9e26e1
Saffat Hasan <saffathasan@gmail.com>
2018-03-03 13:28:56 -0500

    Added event filter form; Added comments to server.js

--- Prototype/server.js
+++ Prototype/server.js
@@ -1,3 +1,4 @@
+/** Required modules and files **/
 var http = require('http');
 var express = require('express');
 var pug = require('pug');
@@ -6,6 +7,7 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var mailer = require('./mailer.js');
 
+/** Initialize Application **/
 app = express();
 app.set('view engine', 'pug');
 app.use(express.static('.'));
@@ -15,15 +17,15 @@ app.use(session({
     saveUninitialized: true
 	})
 );
-// Allows use of #{session.user}
 app.use(function(req,res,next){
-    res.locals.session = req.session;
+    res.locals.session = req.session; // Links req.session to local session variable
     next();
 });
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
+    /** Landing page. Lists up to 4 events **/
     var db = database.get_connection();
     db.child('events').limitToFirst(4).once('value', function(snapshot) {
 		res.render('index', {"events": snapshot.val()});
@@ -32,7 +34,18 @@ app.get('/', function(req, res) {
 });
 
 app.get('/events', function(req,res) {
-    /** List all events and filter based on GET params **/
+    /** List all events and filter based on GET params because
+		firebase does not natively support LIKE querying,
+		it is manually implemented.
+
+	ARGS
+		name (string): events that contain the string
+		start_date (date): events that begin after this date
+		end_date (date): events that end before this date
+
+	RETURNS
+		Page with events within user specified criteria
+	**/
     var name = req.query.name;
     var start_date = req.query.start_date;
     var end_date = req.query.end_date;
@@ -71,6 +84,7 @@ app.get('/events', function(req,res) {
                 }
             }
         }
+		// Only events that match criteria remain in the events object
 		res.render('Events', {"events": events});
 		res.end();
     });
@@ -79,19 +93,21 @@ app.get('/events', function(req,res) {
 function dateToMilliSeconds(date) {
     /** Returns YYYY-MM-DD as time in milliseconds
 
-    ARGS: input (string): YYYY-MM-DD
+    ARGS
+		date (string): YYYY-MM-DD
 
-    RETURNS:
-        integer: MS since epoch and input date **/
+    RETURNS
+        Milliseconds (integer) since epoch and input
+	**/
     var parts = input.split('-');
     return new Date(parts[0], parts[1]-1, parts[2]).getTime();
 }
 
 app.get('/view/:id', function(req,res) {
+	/** Get an event by specified ID. Return error if not found **/
     var db = database.get_connection();
     var event_id = req.params.id;
-    db.child('events').child(event_id)
-        .once('value')
+    db.child('events').child(event_id).once('value')
         .then(function(snapshot) {
             if(snapshot.val()==null) {
                 res.render('event_not_found');
@@ -104,11 +120,13 @@ app.get('/view/:id', function(req,res) {
 });
 
 app.get('/about', function(req,res) {
+	/** About page **/
     res.render('aboutus');
     res.end();
 });
 
 app.get('/login', function(req,res) {
+	/** Login page. Redirects to landing page if already logged in. **/
 	if (req.session.user!==undefined) {
 		res.writeHead(302, {Location: "/"});
 		res.end();
@@ -119,17 +137,20 @@ app.get('/login', function(req,res) {
 });
 
 app.get('/logout', function(req,res) {
+	/** Logout page. Redirects to landing page. **/
 	delete req.session.user;
     res.writeHead(302,  {Location: "/"});
 	res.end();
 });
 
 app.get('/create_event', function(req, res) {
+	/** submit event page **/
     res.render('create_event');
     res.end();
 });
 
 app.get('/register', function(req, res) {
+	/** User register page. Redirects to landing page if already logged in. **/
 	if (req.session.user!==undefined) {
 		res.writeHead(302, {Location: "/"});
 		res.end();
@@ -139,18 +160,24 @@ app.get('/register', function(req, res) {
 });
 
 app.get('/confirm/:user_id/:secret', function(req, res) {
-	// Confirms an email
+	/** Confirm user page
+
+	ARGS
+		user_id: firebase generated ID
+		secret: randomly generated number saved in firebase/users/:id/secret
+
+	RETURNS
+		user_confirmed (if valid id/secret combination specified)
+		user_not_confirmed otherwise
+	**/
 	var db = database.get_connection();
 	var user_id = req.params.user_id;
 	var secret = req.params.secret;
 	db.child('users').child(user_id)
 		.once('value')
 		.then(function(snapshot) {
-			if (snapshot.val() == null) {
+			if (snapshot.val() == null || snapshot.val().secret != secret) {
 				res.render('user_not_confirmed');
-				res.end();
-			} else if (snapshot.val().secret != secret) {
-                res.render('user_not_confirmed');
 				res.end();
 			} else {
 				database.confirm_user(user_id);
@@ -162,6 +189,15 @@ app.get('/confirm/:user_id/:secret', function(req, res) {
 });
 
 app.post('/is_unique_email', function(req,res) {
+	/** Ajax query that checks if an email is taken
+
+	ARGS
+		email (string): email to be checked
+
+	RETURNS
+		true (boolean) if email is not taken
+		'Email is taken!' otherwise
+	**/
 	var email = req.body.email.toLowerCase();
 	var db = database.get_connection().child('users').orderByChild('email').equalTo(email);
 	db.once('value', function(snapshot) {
@@ -176,13 +212,24 @@ app.post('/is_unique_email', function(req,res) {
 });
 
 app.post('/submit_event', function(req, res) {
+	/** #eventForm endpoint, creates an event by passing all parameters in req.body
+
+	Event fields are populated based on form input tags.
+	**/
 	// TODO modify start/end date to date objects combining date and time
+	// e.g. req.body.start_date = new Date(req.body.start_date);
     var new_event_id = database.create_event(req.body);
     res.send('Success.');
     res.end();
 });
 
 app.post('/submit_user', function(req, res) {
+	/** Creates a new user and sends an email asking user to confirm their identity.
+		Also creates and stores a random number from 1-100000.
+
+	ARGS
+		email (string): user's email address
+	**/
     req.body.secret = Math.floor(Math.random()* 100000) + 1;
 	req.body.email = req.body.email.toLowerCase();
     var new_user_id = database.create_user(req.body);
@@ -205,18 +252,16 @@ app.post('/submit_user', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
+	/** Login form endpoint **/
 	var password = req.body.password;
 	// minimal user input sanitizing
 	req.body.email = req.body.email.toLowerCase();
 	var db = database.get_connection();
 	db.child('users').orderByChild('email').equalTo(req.body.email)
 		.on('value', function(snapshot) {
-			var user;
-			for (item in snapshot.val()) {
-				// really stupid way of getting the user
-				user = snapshot.val()[item];
-				break;
-			}
+			var users = snapshot.val();
+			var user_id = Object.keys(users)[0];
+			var user = users[user_id];
 			// check if password matches and user is confirmed
 			if (user !== undefined && user.password === password && user.is_confirmed === true) {
 				req.session.user = user;
